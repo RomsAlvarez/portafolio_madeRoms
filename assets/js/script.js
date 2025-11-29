1e5581ca9c7471d81d4b81b376bac7d12926d4f1
Romina Alvarez <romina.alvarezloyola@gmail.com>
2025-11-29 20:45:44 -0300

    hola

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+const toggleButton = document.querySelector('.menu-toggle');
+const navMenu = document.querySelector('.nav-menu');
+
+toggleButton.addEventListener('click', () => {
+  navMenu.classList.toggle('show');
+});
